47caf004c69e8cbde97d32f741e96d176d77cec7
serafima8 <147659413+serafima8@users.noreply.github.com>
2023-11-01 00:08:50 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        //Задание 1. Написать метод, «рисующий» на экране горизонтальную линию из 60 символов «*».
+        //static void drawing()
+        //{
+        //    for (int i = 1; i <= 60; i++)
+        //    {
+        //        Console.Write("*");
+        //    }
+        //}
+        //static void Main(string[] args)
+        //{
+        //    drawing();
+        //    Console.ReadKey();
+        //}
+
+
+        //Задание 2. Написать метод, «рисующий» прямоугольный треугольник указанного вида.
+        //static void drawing()
+        //{
+        //    for (int i = 0; i <= 8; i++)
+        //    {
+        //        for (int a = 0; a <= i; a++)
+        //        {
+        //            Console.Write("#");
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //}
+        //static void Main(string[] args)
+        //{
+        //    drawing();
+        //    Console.ReadKey();
+        //}
+
+
+        // Задание 3. Вычислить значение выражения, определив и использовав дополнительный метод.
+        //public static double Func5(double x)
+        //{
+        //    x = (Math.Sqrt(5) + 5) / 2;
+        //    return x;
+        //}
+        //public static double Func12(double x)
+        //{
+        //    x = (Math.Sqrt(12) + 12) / 2;
+        //    return x;
+        //}
+        //public static double Func19(double x)
+        //{
+        //   x =  (Math.Sqrt(19) + 19) / 2;
+        //    return x;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    double x = 0;
+        //    double y = Func5(x)+Func12(x)+Func19(x);
+        //    Console.Write($"y = {y}");
+        //    Console.ReadKey();
+        //}
+
+
+        //Задание 4. Нахождение максимальной величины из двух целых переменных a, b.
+        //public static int Magnitude(int a, int b)
+        //{
+
+        //    if (a > b)
+        //    {
+        //        Console.WriteLine($"{a}>{b}");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine($"{b}>{a}");
+        //    }
+        //    int max = Math.Max(a, b);
+        //    return max;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Введите число a: ");
+        //    int a = int.Parse(Console.ReadLine());
+        //    Console.Write("Введите число b: ");
+        //    int b = int.Parse(Console.ReadLine());
+        //    int Z = Magnitude(a, b);
+        //    Console.Write($"Максимальная величина: {Z}");
+        //    Console.ReadKey();
+        //}
+
+    }
+}
